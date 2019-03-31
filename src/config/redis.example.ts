1f32519b4d4a87aeea2b115937575bc1c89a0a40
bibi <me@pannnda.com>
2019-03-31 17:02:10 +0800

    添加配置example

---
+++ src/config/redis.example.ts
@@ -0,0 +1,7 @@
+import { ClientOpts } from 'redis'
+
+export const redis: ClientOpts = {
+  host: '127.0.0.1',
+  port: 6379,
+  db: 0
+}
---
+++ src/config/wechat.example.ts
@@ -0,0 +1,9 @@
+interface Wechat {
+  appid: string
+  secret: string
+}
+
+export const wechat: Wechat = {
+  appid: 'appid',
+  secret: 'secret'
+}
